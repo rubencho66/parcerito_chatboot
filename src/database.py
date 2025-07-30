67e03024f31649f75327c07afe7c968bc1e98770
SebastianPrado <sebastianpv799@gmail.com>
2025-07-29 23:24:14 -0500

    Chat-APi is now alive and bearer validation is on as well

--- src/database.py
+++ src/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from .env import CONNECTION 
 
-CONNECTION = "postgresql://postgres:admin@localhost:5432/PerficientTest"
 engine = create_engine(CONNECTION)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
